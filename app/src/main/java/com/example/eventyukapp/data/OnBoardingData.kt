61a8d0b7be8131ee5d90c7e757b8e6fd2e0bb3c4
Ariyanaasyer <asyer.ariyana@gmail.com>
2024-06-12 09:36:54 +0700

    fix onboarding screen

--- app/src/main/java/com/example/eventyukapp/data/OnBoardingData.kt
+++ app/src/main/java/com/example/eventyukapp/data/OnBoardingData.kt
@@ -6,12 +6,12 @@ import com.example.eventyukapp.model.OnBoardingItem
 object OnBoardingData {
     val onBoardingItems = listOf(
         OnBoardingItem(
-            resId = R.raw.animasi_onboarding1,
+            resId = R.raw.onboardingeventyuk1,
             title = "Temukan Event Sesuai Seleramu",
             description = "Temukan event sesuai seleramu kapanpun dan dimanapun."
         ),
         OnBoardingItem(
-            resId = R.raw.animasi_onboarding2,
+            resId = R.raw.onboardingeventyuk2,
             title = "Kumpulan Event Seru Yang Menantimu",
             description = "Hari Berwarna dengan Event Yuk!"
         ),
