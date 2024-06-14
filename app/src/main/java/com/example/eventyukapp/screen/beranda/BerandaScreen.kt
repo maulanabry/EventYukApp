f89b28ffd47c37d332d9dcddb0cb566081c0631d
maulanabry <maulanabryan567@gmail.com>
2024-06-14 15:49:28 +0700

    Rekomendasi card

--- app/src/main/java/com/example/eventyukapp/screen/beranda/BerandaScreen.kt
+++ app/src/main/java/com/example/eventyukapp/screen/beranda/BerandaScreen.kt
@@ -4,18 +4,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.eventyukapp.data.DummyData
+import com.example.eventyukapp.model.EventItem
+import com.example.eventyukapp.screen.acara.component.AcaraCard
 import com.example.eventyukapp.screen.beranda.component.BannerSection
+import com.example.eventyukapp.screen.beranda.component.RekomendasiCard
 import com.example.eventyukapp.ui.theme.EventYukAppTheme
 
 @Composable
@@ -40,14 +51,31 @@ fun BerandaScreen(
         // Title below the logo
 
         // List of event cards in a grid
-BannerSection()
+        BannerSection()
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            modifier = Modifier.padding( vertical = 5.dp),
+            text = "Rekomendasi",
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Black,
+            color = Color(0xFF2196F3)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        LazyRow {
+            items(events, key = { it.id }) { event ->
+                RekomendasiCard(event = event) {
+                    navController.navigate("detail/${event.id}")
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+        }
     }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BerandaPreview() {
     EventYukAppTheme {
         BerandaScreen(navController = rememberNavController())
     }
--- app/src/main/java/com/example/eventyukapp/screen/beranda/component/RekomendasiCard.kt
+++ app/src/main/java/com/example/eventyukapp/screen/beranda/component/RekomendasiCard.kt
@@ -1,12 +1,88 @@
 package com.example.eventyukapp.screen.beranda.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.eventyukapp.R
+import com.example.eventyukapp.model.EventItem
+import com.example.eventyukapp.screen.acara.component.AcaraCard
 
-
-@Preview(widthDp = 322, heightDp = 140)
 @Composable
-private fun MilestoneSectionPreview() {
-    BannerSection(Modifier)
+fun RekomendasiCard(
+    event: EventItem,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .requiredWidth(120.dp)
+            .requiredHeight(187.dp)
+            .clickable { onClick(event.mapsLink) }
+
+    ) {
+        Image(
+            painter = painterResource(id = event.picture),
+            contentDescription = "poster",
+            modifier = Modifier
+                .requiredWidth(120.dp)
+                .requiredHeight(150.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .align(Alignment.TopCenter)
+        )
+        Text(
+            text = event.name,
+            color = Color(0xff424242),
+            style = TextStyle(
+                fontSize = 10.sp,
+                fontWeight = FontWeight.Bold,
+            ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 20.dp) // Adjust padding as needed
+        )
+    }
+}
+
+@Preview(widthDp = 120, heightDp = 187)
+@Composable
+private fun RekomendasiCardPreview() {
+    RekomendasiCard(
+        event = EventItem(
+            id = 1,
+            name = "Konser Taylor Swift",
+            location = "Stadion Gelora Bung Karno Jakarta",
+            time = System.currentTimeMillis() + 86400000, // 1 hari dari sekarang
+            description = "Konser spektakuler oleh Taylor Swift",
+            picture = R.drawable.img_poster_1,
+            longDescription = "",
+            mapsLink = "https://www.google.com/maps/place/Stadion+Gelora+Bung+Karno"
+        ),
+        onClick = { mapsLink ->
+            // Implement the action you want to perform on click, e.g., opening Google Maps.
+        }
+    )
 }
